41d04d26d25228ce3d895da6bee0e3f547f3bcf7
Maxou42230 <104990169+Maxou42230@users.noreply.github.com>
2022-05-12 17:55:44 +0200

    Update Streamers.php

--- tp noté/model/Streamers.php
+++ tp noté/model/Streamers.php
@@ -1,2 +1,10 @@
 <?php 
 // ~/tp noté/public/Streamers.php
+
+function find_streamer($dbh, $id)
+{
+    $stats = $dbh->prepare('SELECT name, date, minutes_streamed, rank, avg_viewers, max_viewers, hours_watched, followers, views, followers_total, views_total FROM streamers_stats INNER JOIN streamers ON streamers_stats.streamer = streamers.id WHERE streamers_stats.streamer = :id');
+    $stats->execute(["id"=>$id]);
+    $streamer = $stats->fetchAll();
+    return $streamer;
+}
