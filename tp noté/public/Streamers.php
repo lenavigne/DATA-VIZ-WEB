0377917e75f2cbf10c4ef0bab7cf467157ff7819
Maxou42230 <104990169+Maxou42230@users.noreply.github.com>
2022-05-12 11:34:17 +0200

    Update Streamers.php

--- tp noté/public/Streamers.php
+++ tp noté/public/Streamers.php
@@ -1,5 +1,6 @@
 <?php 
-// ~/tp noté/public/Streamers.php 
+// ~/tp noté/public/Streamers.php
+include __DIR__ . '/../database/db.php';
 // include model 
 include __DIR__ . '/../model/Streamers.php'; 
 // include view 
