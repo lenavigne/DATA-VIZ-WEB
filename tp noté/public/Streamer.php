f6eec3b2dc6cca762f9955469beaa6d6357df5b6
lenavigne <104989762+lenavigne@users.noreply.github.com>
2022-05-27 13:51:41 +0200

    Update Streamer.php

--- tp noté/public/Streamer.php
+++ tp noté/public/Streamer.php
@@ -1,10 +1,9 @@
 <?php 
-// ~/tp noté/public/Streamers.php 
+// ~/tp noté/public/Streamer.php 
 // include model 
 include __DIR__ . '/../database/db.php';
 // include model 
-include __DIR__ . '/../model/Streamers.php'; 
-$streamer = find_streamer($dbh, 1);
-var_dump($streamer);
+include __DIR__ . '/../model/Streamer.php'; 
+$id = $_GET['id'];
 // include view 
 include __DIR__ . '/../view/Streamer.php';
