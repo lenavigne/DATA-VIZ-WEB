0e69708ef2a6b09042fd697ad5d90cdbead5138a
Maxou42230 <104990169+Maxou42230@users.noreply.github.com>
2022-06-01 20:12:00 +0200

    Update Streamer.php

--- tp noté/view/Streamer.php
+++ tp noté/view/Streamer.php
@@ -3,7 +3,7 @@
 <!DOCTYPE HTML>
 <html>
     <head>
-        <link rel="stylesheet" href="./Css.css">
+        <link rel="stylesheet" href="./Css2.css">
         <meta http-equiv="content-type" content="text/html;charset=utf-8" />
         <title><?php echo $streamer[0][0]; ?></title>
         <meta name="description" content="liste de streamers">
@@ -51,18 +51,19 @@
 </script>
         
         <h1><?php echo $streamer[0][0]; ?></h1>
-        
+        <img src=".\images_streamers\<?= $streamer[0][0]?>.jpg", alt=<?= $streamer[0][0] ?>>
         <?php foreach ($streamer as $streamerId => $stream) : ?>
-            <p>Date du stream: <?php echo $stream[1]; ?></p>
-            <p>temps du stream (min): <?php echo $stream[2]; ?></p>
-            <p>rang: <?php echo $stream[3]; ?></p>
-            <p>nb moyen de viewers:  <?php echo $stream[4]; ?></p>
-            <p>nb max de viewers: <?php echo $stream[5]; ?></p>
-            <p>nb d'heures visionnées: <?php echo $stream[6]; ?></p>
-            <p>nb de followers: <?php echo $stream[7]; ?></p>
-            <p>nb de views: <?php echo $stream[8]; ?></p>
-            <p>nb total de folowers: <?php echo $stream[9]; ?></p>
-            <p>nb total de views: <?php echo $stream[10]; ?></p>
+          <br>
+            <h2>Date du stream:</h2> <p><?php echo $stream[1]; ?></p>
+            <h2>temps du stream (min):</h2> <p><?php echo $stream[2]; ?></p>
+            <h2>rang:</h2> <p><?php echo $stream[3]; ?></p>
+            <h2>nb moyen de viewers:</h2>  <p><?php echo $stream[4]; ?></p>
+            <h2>nb max de viewers:</h2> <p><?php echo $stream[5]; ?></p>
+            <h2>nb d'heures visionnées:</h2> <p><?php echo $stream[6]; ?></p>
+            <h2>nb de followers:</h2> <p><?php echo $stream[7]; ?></p>
+            <h2>nb de views:</h2> <p><?php echo $stream[8]; ?></p>
+            <h2>nb total de folowers:</h2> <p><?php echo $stream[9]; ?></p>
+            <h2>nb total de views:</h2> <p><?php echo $stream[10]; ?></p>
         
         <?php endforeach; ?>
         
