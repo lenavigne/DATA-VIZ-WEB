eca60c0dd5c64c9dec438feb08b90efb0f4e83ea
YugitoUwU <90696712+YugitoUwU@users.noreply.github.com>
2022-06-01 15:18:52 +0200

    Update Streamers.php

--- tp noté/view/Streamers.php
+++ tp noté/view/Streamers.php
@@ -11,14 +11,37 @@
 
     <body>
         <h1>All streamers</h1>
-
         <?php foreach ($test as $testId=>$streamer): ?>
-        <p>Streamer : <?php  echo $streamer[0]; ?></p>
+            <button id="togg1">Cliquez-moi !</button>
+        <div id="d1">
+            <p>Streamer : <a href="/streamer.php?id=<?=$testId+1; ?>"><?php  echo $streamer[0]; ?></a></p>
         <p>Date du dernier stream : <?php  echo $streamer[1]; ?></p>
         <p>Nb followers total : <?php echo $streamer[2] ?></p>
         <p>Nb views total : <?php echo $streamer[3] ?></p>
-
+        </div>
         <?php endforeach; ?>
 
     </body>
+
+    <script type="text/javascript">
+    let togg1 = document.getElementById("togg1");
+        let d1 = document.getElementById("d1");
+        togg1.addEventListener("click", () => {
+  if(getComputedStyle(d1).display != "none"){
+    d1.style.display = "none";
+  } else {
+    d1.style.display = "block";
+  }
+})
+// function show_hide_div(input_box, div_stats) 
+// {
+//     if(input_box.checked)
+//     {
+//         document.getElementId(div_stats).style.display ="flex";
+//     }else
+//     {
+//         document.getElementId(div_stats).style.display ="none";
+//     }
+// }
+    </script>
 </html>
